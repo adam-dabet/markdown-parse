93dc76a90fcdd82f8f769043a231a5267c907b86
adam-dabet <adamdabet@gmail.com>
2022-02-02 17:49:26 -0800

    fixed errors

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -14,13 +14,14 @@ public class MarkdownParseTest {
     }
     @Test
     public void TestFile1() throws IOException {
-        String contentsTest = Files.readString(Path.of("test-file.md"));
-        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(contentsTest));
+        String contentsTest = Files.readString(Path.of("/Users/adamdabet/Documents/Github/markdown-parse/test1.md"));
+        assertEquals(List.of("https://something.com", "some-page.html", "last-link.html"), MarkdownParse.getLinks(contentsTest));
     }
 
     @Test
     public void TestFile2() throws IOException {
-        String contentsTest = Files.readString(Path.of("test-file2.md"));
+        String contentsTest = Files.readString(Path.of("/Users/adamdabet/Documents/Github/markdown-parse/test2.md"));
         assertEquals(List.of("https://anything.com","another-page.html","a-page.html"), MarkdownParse.getLinks(contentsTest));
     }
+
 }
